c0ee602a1f4b8bb2e3da29fa9212eeb38b3e33be
jaeyz <jc.nuevas@klab.com>
2014-07-08 17:29:07 +0800

    Added UI Sample prefabs

---
+++ Assets/Try/Sample.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sample : MonoBehaviour {
+
+	public GameObject pInstance;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp (KeyCode.KeypadEnter)) {
+			transform.parent.GetComponent<PrefabInstance> ().enabled = true;
+			PrefabInstance.BakeInstance (pInstance.GetComponent<PrefabInstance>());
+		}
+	}
+}
