4fcd5a73eb07932dec4ae679bd9457e555f83643
jaeyz <jc.nuevas@klab.com>
2014-07-10 17:25:10 +0800

    Added Quit Popup

---
+++ Assets/StrangeWorkflow/QuitContext/Scripts/QuitMediator.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using strange.extensions.mediation.impl;
+
+public class QuitMediator : EventMediator {
+
+	[Inject]
+	public QuitView quitView { get; set; }
+
+	public override void OnRegister() {
+		AddListeners ();
+	}
+	
+	public override void OnRemove() {
+		RemoveListeners ();
+	}
+
+	private void AddListeners() {
+		quitView.OnConfirm.AddListener(Confirm);
+		quitView.OnCancel.AddListener(Cancel);
+	}
+
+	private void RemoveListeners() {
+		quitView.OnConfirm.RemoveListener(Confirm);
+		quitView.OnCancel.RemoveListener(Cancel);
+	}
+
+	private void Confirm() {
+		Debug.Log ("Confirmed Quit!");
+	}
+
+	private void Cancel() {
+		SceneChanger.Instance.SwitchScene ("ApplicationScene", quitView.gameObject);
+	}
+}
---
+++ Assets/StrangeWorkflow/QuitContext/Scripts/QuitView.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using strange.extensions.mediation.impl;
+using strange.extensions.signal.impl;
+
+public class QuitView : View {
+
+	[SerializeField]
+	private CustomButton confirmButton;
+
+	[SerializeField]
+	private CustomButton cancelButton;
+
+	public Signal OnConfirm;
+	public Signal OnCancel;
+
+	public QuitView () {
+		OnConfirm = new Signal ();
+		OnCancel = new Signal ();
+	}
+
+	protected override void Start() {
+		AddListeners ();
+	}
+
+	protected override void OnDestroy() {
+		RemoveListeners ();
+	}
+
+	private void AddListeners() {
+		confirmButton.buttonClicked += Confirm;
+		cancelButton.buttonClicked += Cancel;
+	}
+
+	private void RemoveListeners() {
+		confirmButton.buttonClicked -= Confirm;
+		cancelButton.buttonClicked -= Cancel;
+	}
+
+	private void Confirm() {
+		if (OnConfirm != null)
+			OnCancel.Dispatch ();
+	}
+
+	private void Cancel() {
+		if (OnCancel != null)
+			OnCancel.Dispatch ();
+	}
+}
